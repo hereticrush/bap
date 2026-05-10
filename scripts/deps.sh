1ffd2bade33094a807c61b371c7f520ddf3e2732
Tunç Yılmaz Türkoğlu <tunc.yt.93@gmail.com>
2026-05-10 15:32:32 +0300

    fix: tests.sh deps.h cmd

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -12,4 +12,4 @@ docker run --rm \
   -v "$(pwd)":/src \
   -w /src \
   golang:1.24-alpine \
-  sh -c "apk add --no-cache gcc musl-dev > /dev/null && go get github.com/hibiken/asynq && go mod tidy"
+  sh -c "apk add --no-cache gcc musl-dev > /dev/null && go get github.com/mattn/go-sqlite3 && go get github.com/hibiken/asynq && go mod tidy"
